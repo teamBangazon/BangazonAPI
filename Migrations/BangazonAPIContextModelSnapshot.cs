6970929632762af79c9e4b756c1bffca2ba9a3c9
Dylan Smith <jeffdylansmith@gmail.com>
2017-07-26 13:53:04 -0500

    customer resource dev accessability added

--- Migrations/BangazonAPIContextModelSnapshot.cs
+++ Migrations/BangazonAPIContextModelSnapshot.cs
@@ -15,6 +15,28 @@ namespace BangazonAPI.Migrations
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");
 
+            modelBuilder.Entity("BangazonAPI.Models.Customer", b =>
+                {
+                    b.Property<int>("CustomerId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("Active");
+
+                    b.Property<DateTime>("DateCreated")
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired();
+
+                    b.Property<string>("LastName")
+                        .IsRequired();
+
+                    b.HasKey("CustomerId");
+
+                    b.ToTable("Customer");
+                });
+
             modelBuilder.Entity("BangazonAPI.Models.Employee", b =>
                 {
                     b.Property<int>("EmployeeId")
